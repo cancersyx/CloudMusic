9a2a4b79adc368a243006d3b8e2d6b83ec81302f
Zhang ShuoFeng <852333743@qq.com>
2019-04-16 21:07:33 +0800

    just add some imgs

--- NetCloudMusic/app/src/main/java/com/zsf/netcloudmusic/BaseActivity.java
+++ NetCloudMusic/app/src/main/java/com/zsf/netcloudmusic/BaseActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 /**
  * @author EWorld  e-mail:852333743@qq.com
  * 2019/3/18
@@ -13,5 +15,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
     }
 }
--- NetCloudMusic/app/src/main/java/com/zsf/netcloudmusic/MainActivity.java
+++ NetCloudMusic/app/src/main/java/com/zsf/netcloudmusic/MainActivity.java
@@ -1,10 +1,13 @@
 package com.zsf.netcloudmusic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+
+import butterknife.BindView;
 
 public class MainActivity extends BaseActivity {
-
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
